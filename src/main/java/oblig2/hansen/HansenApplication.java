cb4364743d4c4bd7ccbce2a6a011fc3c58b2a9ae
githansen <yohanhansen97@gmail.com>
2021-02-18 14:14:25 +0100

    First commit

---
+++ src/main/java/oblig2/hansen/HansenApplication.java
@@ -0,0 +1,13 @@
+package oblig2.hansen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HansenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oblig2/hansen/HansenApplicationTests.java
@@ -0,0 +1,13 @@
+package oblig2.hansen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
